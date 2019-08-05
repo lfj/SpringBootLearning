bedf21c31f8987a342952682f5a2160bf8358e2e
lifj <lifj@dtdream.com>
2019-08-05 23:44:20 +0800

    学习Spring AOP打印日志 	new file:   src/main/java/com/lfj/demo/aspect/WebLog.java 	new file:   src/main/java/com/lfj/demo/aspect/WebLogAspectJ.java 	new file:   src/main/java/com/lfj/demo/service/SelfRunner.java 	modified:   pom.xml 	modified:   src/main/java/com/lfj/demo/aspect/WebLog.java 	modified:   src/main/java/com/lfj/demo/aspect/WebLogAspectJ.java 	modified:   src/main/java/com/lfj/demo/controller/GetGirlsController.java 	modified:   src/main/java/com/lfj/demo/service/SelfRunner.java

---
+++ src/main/java/com/lfj/demo/aspect/WebLogAspectJ.java
@@ -0,0 +1,7 @@
+package com.lfj.demo.aspect;
+
+/**
+ * Created by lfj-pc on 2019/7/28.
+ */
+public class WebLogAspectJ {
+}
---
+++ src/main/java/com/lfj/demo/service/SelfRunner.java
@@ -0,0 +1,7 @@
+package com.lfj.demo.service;
+
+/**
+ * Created by lfj-pc on 2019/7/27.
+ */
+public class SelfRunner {
+}
